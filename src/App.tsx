37bd0bc76ab489e095c255e747d6bcf8ddaf9548
thedotmack <thedotmack@gmail.com>
2025-03-28 20:00:08 +0000

    removed unused stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-// import FGIStrategyComparison_v4 from './components/FGIStrategyComparison_v4'
 import TradingBotUI from './components/TradingBotUI'
 import './App.css'
 
@@ -7,7 +6,6 @@ import './App.css'
 function App () {
   return (
     <>
-      {/* <FGIStrategyComparison_v4 /> */}
       <TradingBotUI />
     </>
   )
